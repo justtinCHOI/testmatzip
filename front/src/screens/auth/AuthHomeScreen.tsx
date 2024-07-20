d903d862e7ba7a19be04fc602b3b8db52e66158c
justtinCHOI <nicolao011113@gmail.com>
2024-07-20 14:53:27 +0900

    feature HomeScreen

--- front/src/screens/auth/AuthHomeScreen.tsx
+++ front/src/screens/auth/AuthHomeScreen.tsx
@@ -1,9 +1,10 @@
 import {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
-import {Button, StyleSheet, View} from 'react-native';
+import {Dimensions, StyleSheet, View, Image} from 'react-native';
 import {AuthStackParamList} from '../../navigations/stack/AuthStackNavigator';
 import {authNavigations} from '../../constants/navigations';
 import CustomButton from '../../components/CustomButton';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
 type AuthHomeScreenProps = StackScreenProps<
   AuthStackParamList,
@@ -12,20 +13,47 @@ type AuthHomeScreenProps = StackScreenProps<
 
 function AuthHomeScreen({navigation}: AuthHomeScreenProps) {
   return (
-    <View>
-      <CustomButton
-        label="로그인하기"
-        onPress={() => navigation.navigate(authNavigations.LOGIN)}
-      />
-      <CustomButton
-        label="회원가입하기"
-        variant="outlined"
-        onPress={() => navigation.navigate(authNavigations.SIGNUP)}
-      />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image
+          resizeMode="contain"
+          style={styles.image}
+          source={require('../../assets/matzip.png')}
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <CustomButton
+          label="로그인하기"
+          onPress={() => navigation.navigate(authNavigations.LOGIN)}
+        />
+        <CustomButton
+          label="회원가입하기"
+          variant="outlined"
+          onPress={() => navigation.navigate(authNavigations.SIGNUP)}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 1.5,
+    alignItems: 'center',
+  },
+  imageContainer: {
+    flex: 1.5,
+    width: Dimensions.get('screen').width / 2,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+  buttonContainer: {
+    flex: 1,
+    gap: 10,
+  },
+});
 
 export default AuthHomeScreen;
