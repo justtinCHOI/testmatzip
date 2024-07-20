42fe1f4c1ab8ca596f470ee76df27f4ff52e5b0c
justtinCHOI <nicolao011113@gmail.com>
2024-07-20 18:02:06 +0900

    update form UX

--- front/src/screens/auth/LoginScreen.tsx
+++ front/src/screens/auth/LoginScreen.tsx
@@ -1,11 +1,13 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import {SafeAreaView, StyleSheet, View, Text} from 'react-native';
 import InputField from '../../components/InputField';
 import CustomButton from '../../components/CustomButton';
 import useForm from '../../hooks/useForm';
 import {validateLogin} from '../../utils';
+import {TextInput} from 'react-native-gesture-handler';
 
 function LoginScreen() {
+  const passwordRef = useRef<TextInput | null>(null);
   const handleSubmit = () => {
     console.log('values', login.values);
   };
@@ -19,17 +21,24 @@ function LoginScreen() {
     <SafeAreaView style={styles.container}>
       <View style={styles.inputContainer}>
         <InputField
+          autoFocus
           placeholder="이메일"
           error={login.errors.email}
           touched={login.touched.email}
           inputMode="email"
+          returnKeyType="next"
+          blurOnSubmit={false}
+          onSubmitEditing={() => passwordRef.current?.focus()}
           {...login.getTextInputProps('email')}
         />
         <InputField
+          ref={passwordRef}
           placeholder="비밀번호"
           error={login.errors.password}
           touched={login.touched.password}
           secureTextEntry
+          returnKeyType="join"
+          blurOnSubmit={false}
           {...login.getTextInputProps('password')}
         />
       </View>
