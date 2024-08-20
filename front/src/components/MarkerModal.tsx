0e18c31dc6a3345b1dce2f521b892e11b3306112
justtinCHOI <nicolao011113@gmail.com>
2024-08-21 02:57:21 +0900

    add env setting

--- front/src/components/MarkerModal.tsx
+++ front/src/components/MarkerModal.tsx
@@ -110,7 +110,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
-  },
+  }, 
   imageContainer: {
     width: 70,
     height: 70,
